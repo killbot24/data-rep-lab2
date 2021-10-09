8c10dbfe11b8c75fe9ae32f131929e73c1766bb3
thomas burke <piecraft24@gmail.com>
2021-10-09 18:50:00 +0100

    Rebuilt and commented code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
+import { Component } from 'react';
+import { Header} from './componets/header'
+import { Footer} from './componets/footer'
+import { Content} from './componets/content'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar,Nav,Container}from 'react-bootstrap';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+class App extends Component {
+  render (){
+    return(
+      <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<Navbar bg="primary" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/read">read</Nav.Link>
+      <Nav.Link href="/create">Create</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  <br/>
+  <Switch>
+<Route path='/' component={Content} exact/>//loads the content
+<Route path='/Create' component={Header} exact/>//loads the header
+<Route path='/read' component={Footer} exact/>//loads the footer
+
+  </Switch>
+
+     
+    </div></Router>
   );
+}
 }
 
 export default App;
